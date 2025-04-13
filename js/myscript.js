3995d46c75976810685e0cfd68cf13183ee52293
melvinkentc <melvinkentc@gmail.com>
2025-04-13 10:19:14 +0800

    april 13

---
+++ js/myscript.js
@@ -0,0 +1,10 @@
+(function(d ) {
+   'use strict'; 
+  const icon = d.querySelector( '.icon' );
+  const theBody = d.querySelector('body');
+   
+   icon.addEventListener( 'click', function() { 
+   	theBody.classList.toggle('menu-open');
+    });
+
+}( document ));
